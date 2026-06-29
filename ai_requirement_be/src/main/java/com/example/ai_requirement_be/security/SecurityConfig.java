9b0f06476a511693906b595a1de705adad89bf4f
hunglocker <hungdzai404@gmail.com>
2026-06-29 16:41:48 +0700

    Demo testcase Controller

--- ai_requirement_be/src/main/java/com/example/ai_requirement_be/security/SecurityConfig.java
+++ ai_requirement_be/src/main/java/com/example/ai_requirement_be/security/SecurityConfig.java
@@ -9,13 +9,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
-import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
--- ai_requirement_be/src/test/java/com/example/ai_requirement_be/controller/AuthControllerTest.java
+++ ai_requirement_be/src/test/java/com/example/ai_requirement_be/controller/AuthControllerTest.java
@@ -3,6 +3,7 @@ package com.example.ai_requirement_be.controller;
 import com.example.ai_requirement_be.controller.Authendication.AuthController;
 import com.example.ai_requirement_be.dto.Auth.AuthResponseDTO;
 import com.example.ai_requirement_be.dto.Auth.LoginRequestDTO;
+import com.example.ai_requirement_be.dto.Auth.RegisterRequestDTO;
 import com.example.ai_requirement_be.repository.IUserRepository;
 import com.example.ai_requirement_be.service.Authendication.AuthService;
 import com.example.ai_requirement_be.service.Authendication.JwtService;
@@ -19,9 +20,9 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(AuthController.class)
@@ -44,11 +45,64 @@ public class AuthControllerTest {
 
     private LoginRequestDTO  validLoginRequestDTO;
 
+    private RegisterRequestDTO  validRegisterRequestDTO;
+
+
+
+
     @BeforeEach
     void setUp() {
         validLoginRequestDTO = new LoginRequestDTO();
         validLoginRequestDTO.setEmail("lukaku@gmail.com");
         validLoginRequestDTO.setPassword("lukaku123");
+        validRegisterRequestDTO = new RegisterRequestDTO();
+        validRegisterRequestDTO.setEmail("lookatme@gmail.com");
+        validRegisterRequestDTO.setPassword("lookatme123");
+    }
+
+
+
+    @Test
+    @DisplayName("Nên đăng ký thành công khi email và password hợp lệ")
+    void register_Sucess() throws Exception {
+        String mockMessage = "Đăng ký tài khoản thành công!";
+
+        Mockito.when(authService.register(Mockito.any(RegisterRequestDTO.class)))
+                .thenReturn(mockMessage);
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(validLoginRequestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Đăng ký tài khoản thành công!"));
+    }
+
+    @Test
+    @DisplayName("Nên trả về lỗi 400 khi để trống emali hoặc mật khẩu lúc đăng ký ")
+    void register_Failure_BlankField() throws Exception {
+        RegisterRequestDTO  registerRequestDTO = new RegisterRequestDTO();
+        registerRequestDTO.setEmail("");
+        registerRequestDTO.setPassword(" ");
+
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(registerRequestDTO)))
+                .andExpect(status().isBadRequest());
+                Mockito.verifyNoInteractions(authService);
+    }
+
+    @Test
+    @DisplayName("Nên trả về lỗi 400 khi nhập email sai định dạng lúc đăng ký")
+    void register_Failure_InvalidEmail() throws Exception {
+        RegisterRequestDTO invalidRequest = new RegisterRequestDTO();
+        invalidRequest.setEmail("sai_dinh_dang_email.com");
+        invalidRequest.setPassword("password123");
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidRequest)))
+                .andExpect(status().isBadRequest());
+
+        Mockito.verifyNoInteractions(authService);
     }
 
     @Test
@@ -81,7 +135,6 @@ public class AuthControllerTest {
                 .andExpect(status().isBadRequest());
                 Mockito.verifyNoInteractions(authService);
     }
-
     @Test
     @DisplayName("Nên trả về lỗi 400 Bad Request khi nhập Email sai định dạng")
     void login_BadRequest_Email() throws Exception {
